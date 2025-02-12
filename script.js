554678554db76639acd1b8fc7ea6c0d7ec3cd4a0
rahulvb-bit <rahul.borade@parseq.com>
2025-02-12 12:13:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const alertButton = document.getElementById('alertButton');
+    alertButton.addEventListener('click', function() {
+        alert('Button was clicked!');
+    });
+});
